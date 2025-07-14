c294562759e94b0037534fa13795f051f6e510dc
OrcaniaDevelops <mason_ross2@icloud.com>
2025-07-13 22:21:39 -0400

    Update RNGFunctions.lua
    Added example

--- RNGFunctions.lua
+++ RNGFunctions.lua
@@ -1,7 +1,30 @@
 
 local RNGFunctions = {}
 
-function RNGFunctions:ChooseRandomPart(objects: table, rarities: table)
+
+--[[ Example:
+
+    local objects = {
+		workspace.Basic,
+		workspace.Rare,
+		workspace.Legendary
+	}
+
+	local rarities = {
+		["Basic"] = 70,
+		["Rare"] = 25,
+		["Legendary"] = 5
+	}
+	
+	local RNGFunctions = require(location.ScriptName)
+	
+	local newChoice = RNGFunctions:ChooseRandom(objects, rarities)
+	print(newChoice)
+		
+		
+
+--]]
+function RNGFunctions:ChooseRandom(objects: table, rarities: table)
 
     local randomNum = math.random(1,100)
     local count = 0
