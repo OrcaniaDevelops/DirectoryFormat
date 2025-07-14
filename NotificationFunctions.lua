7e2ae593806c5e900dadd796e60d4ccd3b6ddfe6
OrcaniaDevelops <mason_ross2@icloud.com>
2025-07-14 14:10:27 -0400

    Update NotificationFunctions.lua

--- NotificationFunctions.lua
+++ NotificationFunctions.lua
@@ -2,9 +2,66 @@
 -- Scripted by Orcania
 -- Created 7/14/25
 
+--Services--
+
+local TweenService = game:GetService("TweenService")
+local SoundService = game:GetService("SoundService")
+
+--Variables--
+
+--Tweens--
+
+
+
+---MODULE---
+
+
+
 local NotificationFunctions = {}
 
+--Functions--
+
+function NotificationFunctions:CreateNewNotification(player: Player, pos: string, text: string, duration: number, color: Color3)
+
+	local playerGui = player.PlayerGui
+	local NotifGui = playerGui.Notifications
+
+	local SFXFolder = SoundService.SFX
+
+	local NotifSound = SFXFolder.Notification:Clone()
+	NotifSound.Parent = playerGui
+	local playbackSpeed = math.random(0.8, 1.2)
+	NotifSound.PlaybackSpeed = playbackSpeed
+
+    local self = script
 
 
+	local Notification = self.Template:Clone()
+    local TweenIn = TweenInfo.new(Notification, 0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.In)
+	local TweenOut = TweenInfo.new(Notification, 0.2, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
+
+	local Frame = NotifGui:FindFirstChild(pos)
+
+	if not Frame then
+		warn(pos .. " is not a valid member of " .. player.Name .. "'s Notification UI.")
+	end
+	
+	Notification.Parent = NotifGui[pos] 
+	NotifSound:Play()
+	Notification.TextLabel.Text = text
+	Notification.TextLabel.TextColor = color
+
+	Notification.UIScale.Scale = 0
+	TweenService:Create(Notification.UIScale, TweenIn, {Scale = 1}):Play()
+
+	task.delay(duration, (function()
+
+		TweenService:Create(Notification.UIScale, TweenOut, {Scale = 0}):Play()
+		task.wait(0.2)
+		Notification:Destroy()
+
+	end)
+
+end)
 
 return NotificationFunctions
