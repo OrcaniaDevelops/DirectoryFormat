3e9c3fa09f5c062735625b11ee4bbf294a1968a0
OrcaniaDevelops <mason_ross2@icloud.com>
2025-07-13 22:16:22 -0400

    Update RNGFunctions.lua
    Basic RNG functions, compatible with rarities

--- RNGFunctions.lua
+++ RNGFunctions.lua
@@ -1,1 +1,23 @@
 
+local RNGFunctions = {}
+
+function RNGFunctions:ChooseRandomPart(objects: table, rarities: table)
+
+    local randomNum = math.random(1,100)
+    local count = 0
+	
+	for rarity, weight in rarities do
+
+		counter += weight
+
+		if randomNum <= counter then
+			return objects[rarity]
+        end
+
+	end)
+
+end)
+
+return RNGFunctions
+
+
